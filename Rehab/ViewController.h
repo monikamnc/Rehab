67f0eb2472fe07546c792dd1cd31edcedd3d4dd7
MónicaMartín <monica.martin1@uvic.cat>
2014-03-10 17:03:30 +0100

    Putu Pou

--- Rehab/ViewController.h
+++ Rehab/ViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *cap;
+@property (strong, nonatomic) IBOutlet UIImageView *imatgeCos;
 
 @end
