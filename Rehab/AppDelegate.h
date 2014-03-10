2ed8c57ac11141f049513539ec406fa3b7d55066
MónicaMartín <monica.martin1@uvic.cat>
2014-03-10 15:21:40 +0100

    Initial Commit

---
+++ Rehab/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rehab
+//
+//  Created by monica.martin1 on 10/03/14.
+//  Copyright (c) 2014 monica.esteve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rehab/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rehab
+//
+//  Created by monica.martin1 on 10/03/14.
+//  Copyright (c) 2014 monica.esteve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
